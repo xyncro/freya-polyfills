e68d3927a6429cdab6154113fd3f73302851b9e3
kolektiv <andrew@xyncro.com>
2016-03-04 01:32:06 +0000

    basic katana polyfill (should handle IIS and Self Host)

---
+++ src/Freya.Polyfills.Katana/Polyfills.Katana.fs
@@ -0,0 +1,37 @@
+﻿module Freya.Polyfills.Katana
+
+open System.Net
+open System.Web
+open Freya.Core
+open Freya.Core.Operators
+open Freya.Polyfills
+
+(* HttpListener *)
+
+let private httpListener_ =
+        State.value_<HttpListenerContext> "System.Net.HttpListenerContext"
+
+let private httpListener =
+        !. httpListener_
+    >>= function | Some x -> Request.rawPathAndQuery_ .= Some x.Request.RawUrl
+                 | _ -> Freya.empty
+
+(* HttpContext *)
+
+let private httpContext_ =
+        State.value_<HttpContextWrapper> "System.Web.HttpContextBase"
+
+let private httpContext =
+        !. httpContext_
+    >>= function | Some x -> Request.rawPathAndQuery_ .= Some x.Request.Url.PathAndQuery
+                 | _ -> Freya.empty
+
+(* Polyfill *)
+
+[<RequireQualifiedAccess>]
+module Polyfill =
+
+    let katana =
+            httpListener
+         *> httpContext
+         *> Pipeline.next
---
+++ src/Freya.Polyfills/Polyfills.fs
@@ -0,0 +1,11 @@
+﻿module Freya.Polyfills
+
+open Freya.Core
+
+(* Optics *)
+
+[<RequireQualifiedAccess>]
+module Request =
+
+    let rawPathAndQuery_ =
+        State.value_<string> "owin.RawPathAndQuery"
