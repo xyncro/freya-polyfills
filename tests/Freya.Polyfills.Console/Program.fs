0c3ba4e600bcce712a2e180c229b863453d51ef5
kolektiv <andrew@xyncro.com>
2016-06-05 20:02:35 +0100

    interim kestrel investigation

---
+++ tests/Freya.Polyfills.Console/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+open System.IO
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Hosting
+
+type Startup() =
+    member __.Configure (app: IApplicationBuilder) =
+        app.Run (fun x ->
+            printfn "%A - %A - %A - %A" x.Request.Method x.Request.PathBase x.Request.Path x.Request.QueryString
+            x.Response.WriteAsync("Hello world"))
+
+[<EntryPoint>]
+let main args =
+
+    try
+        WebHostBuilder()
+            .UseKestrel()
+            .UseUrls([| "http://localhost:7000" |])
+            //.UseContentRoot(Directory.GetCurrentDirectory())
+            //.UseDefaultHostingConfiguration(args)
+            .UseStartup<Startup>()
+            .Build()
+            .Run()
+    with
+        | ex ->
+            printfn "%A" ex
+
+    0
